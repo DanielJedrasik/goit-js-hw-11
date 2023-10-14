aa02b3317856ddffa441793c0a695fcb70ae6ef4
DanielJedrasik <djedrasik3@gmail.com>
2023-10-14 23:26:33 +0200

    update 3.2

--- src/api.js
+++ src/api.js
@@ -3,9 +3,8 @@ import axios from 'axios';
 import Notiflix from 'notiflix';
 const apiKey = '39999567-6a63dde8da73c54260ff5c1f8';
 
-async function fetchImages(query, page, btnLoadMore, perPage) {
+export async function fetchImages(query, page, btnLoadMore, perPage) {
   const url = `https://pixabay.com/api/?key=${apiKey}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
-
   try {
     const response = await axios.get(url);
     const { data } = response;
@@ -16,12 +15,9 @@ async function fetchImages(query, page, btnLoadMore, perPage) {
       btnLoadMore.style.display = 'none';
       return null;
     }
-
     return data;
   } catch (error) {
     Notiflix.Notify.failure('Oops! Something went wrong. Please try again.');
     return null;
   }
 }
-
-export { fetchImages };
